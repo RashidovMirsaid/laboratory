466c994deeffe5f2afebea2507e624190c9927c9
Mirsaid Rashidov <mirsaidrasidov1@gmail.com>
2023-12-26 18:00:38 +0600

    добавлена игра покер где я использовал useState, map, и добавил функционал для кнопки когда при нажатии ее создаются рандомные карты

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import Cards from './components/Cards/Cards';
+import CardDeck from './lib/CardDeck';
+import Card from './lib/Card';
 
 function App() {
+	const [cards, setCards] = useState<Card[]>([]);
+
+	const dealCards = () => {
+		const deck = CardDeck.createDeck();
+		const dealtCards: Card[] = [];
+
+		for (let i = 0; i < 5; i++) {
+			const card = deck.getCard();
+			if (card) {
+				dealtCards.push(card);
+			}
+		}
+
+		setCards(dealtCards);
+	};
+
 	return (
 		<div className='App'>
-			<Cards rank='K' suit='diams' />
+			<button onClick={dealCards}>Раздать карты</button>
+			{cards.length > 0 && (
+				<div className='playingCards faceImages'>
+					{cards.map((card) => (
+						<Cards suit={card.suit} rank={card.rank} />
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
