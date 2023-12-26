6dd2566244ea69a03c2cd85dd2593a68c4c6b785
Mirsaid Rashidov <mirsaidrasidov1@gmail.com>
2023-12-26 17:32:50 +0600

    Deleted publuc properties

--- src/lib/Card.ts
+++ src/lib/Card.ts
@@ -1,38 +1,11 @@
 class Card {
-	suit: 'diams' | 'spades' | 'hearts' | 'clubs';
-	rank:
-		| '2'
-		| '3'
-		| '4'
-		| '5'
-		| '6'
-		| '7'
-		| '8'
-		| '9'
-		| '10'
-		| 'J'
-		| 'Q'
-		| 'K'
-		| 'A';
-	constructor(
-		rank:
-			| '2'
-			| '3'
-			| '4'
-			| '5'
-			| '6'
-			| '7'
-			| '8'
-			| '9'
-			| '10'
-			| 'J'
-			| 'Q'
-			| 'K'
-			| 'A',
-		suit: 'diams' | 'spades' | 'hearts' | 'clubs'
-	) {
-		this.suit = suit;
+	rank: string;
+	suit: 'diams' | 'hearts' | 'clubs' | 'spades';
+
+	constructor(rank: string, suit: 'diams' | 'hearts' | 'clubs' | 'spades') {
 		this.rank = rank;
+		this.suit = suit;
 	}
 }
+
 export default Card;
